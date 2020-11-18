b4279e111e16a9233acc0b2e8db65a3aab7e7c81
Masa Utkovic <masa.utkovic@gmail.com>
2020-11-18 18:28:58 +0100

    add pino-http logging

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const pino = require('pino-http')();
+
 const app = express();
+app.use(pino);
 app.use(express.static('./public'));
 
-app.listen(process.env.PORT || 8080, () => console.log("server alive and kickin!"));
+app.listen(process.env.PORT || 8080, () => console.log(`server alive and kickin on ${process.env.PORT || 8080}!`));
